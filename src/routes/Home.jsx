1ffd93c571e54a701663f193d1748539d3e84506
L.A.S.S. Kularathna <sujeewasandeepa@protonmail.ch>
2023-03-13 13:54:59 +0530

    Changed the font size of my projects page

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -4,50 +4,25 @@ import pic1 from "../assets/dashboard-not-logged-in.png";
 export default function Home () {
     return (
         <>
-            <div className="bg-gradient-to-t from-slate-400 via-slate-200 to-white h-100 mt-0 pb-5">
-                <h1 className="text-7xl font-bold text-center text-slate-500 pt-5">My Projects</h1>
-                <div className='px-10 mt-10 flex sm:mx-0 lg:mx-8'>
-                    <Card
-                        title="Attendy"
-                        link="https://github.com/Team-4X"
-                        description="A web based attendance management system. Our goal is to digitalize
-                        the process of attendance management in educational institutes (mainly schools). To
-                        save the valuable time of students and teachers. So they can focus on more important
-                        tasks."
-                        myRole="Project Manager / developer"
-                        myRoleDesc="I managed the git repository, assigned tasks to my team members and implemented
-                        many parts of system."
-                        workList={['Authentication', 'Marking Attendance', 'Managing Staff members', 'side bar']}
-                        imageSrc={pic1}
-                    />
 
-                </div>
-                <div className='px-10 mx-10 flex sm:flex-col sm:mx-0 lg:mx-8 lg:flex-row'>
-                    <Card
-                        title="Portfolio Website"
-                        link=""
-                        description="fdfd"
-                        myRole="fdfd"
-                        myRoleDesc="fdfd"
-                        workList={["ffddf", "fdfd"]}
-                        imageSrc={pic1}
-                    />
-                    <Card
-                        title="Attendy"
-                        link="https://github.com/Team-4X"
-                        description="A web based attendance management system. Our goal is to digitalize
-                        the process of attendance management in educational institutes (mainly schools). To
-                        save the valuable time of students and teachers. So they can focus on more important
-                        tasks."
-                        myRole="Project Manager / developer"
-                        myRoleDesc="I managed the git repository, assigned tasks to my team members and implemented
-                        many parts of system."
-                        workList={['Authentication', 'Marking Attendance', 'Managing Staff members', 'side bar']}
-                        imageSrc={pic1}
-                    />
+            {/* <h1 className='sm:text-5xl md:text-8xl lg:text-9xl text-center font-bold text-slate-500 m-10'>My Projects</h1>
+             */}
+            <h1 className='text-6xl md:text-8xl lg:text-9xl text-center font-bold text-slate-500 m-10'>My Projects</h1>
 
+            
+            <div className='mx-10 grid lg:grid-cols-2 gap-10 md:grid-cols-2 sm-grid-cols-1'>
+
+                <div className=''>
+                    <Card></Card>
                 </div>
                 
+                <div className=''>
+                    <Card></Card>
+                </div>
+
+                <div className=''>
+                    <Card></Card>
+                </div>
 
             </div>
         </>
